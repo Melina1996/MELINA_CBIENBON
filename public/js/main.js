9ffaeec83b019296b7e0fdadb5f4861075307ef5
Melina1996 <melina.schlachter@gmx.de>
2023-12-08 13:34:18 +0100

    addedJS

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+//links id//
+// let menustarters = document.querySelector("#")
+
+// let starters divs mit jeweiliger id 
+// ...
+// salads
+// ...
+// specialtiy
+// ...
+
+// menustarters.addEventListener("click",()=>{
+//     dishSaalds1.classList.toggle("disappear")
+//     ...
+// })
+
+// für alle meine Links
+// menustarters.addEventListener("click",()=>{
+//     dishSaalds1.classList.toggle("disappear")
+//     ...
+// })
+
